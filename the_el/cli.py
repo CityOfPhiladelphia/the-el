992f802c7b2b561587bef8de92f25191d1e736c8
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-04-19 13:43:10 -0400

    Tested against postgres

--- the_el/cli.py
+++ the_el/cli.py
@@ -23,14 +23,18 @@ def create_storage_adaptor(connection_string, db_schema, geometry_support):
     storage = Storage(engine, dbschema=db_schema, geometry_support=geometry_support)
     return storage
 
-def fopen(file, mode='rb'):
+def fopen(file, mode='r'):
     if file == None:
-        if mode == 'rb':
+        if mode == 'r':
             return sys.stdin
-        elif mode == 'wb':
+        elif mode == 'w':
             return sys.stdout
     else:
         return smart_open(file, mode=mode)
+
+def get_table_schema(table_schema_path):
+    with fopen(table_schema_path) as file:
+        return json.load(file)
 
 @main.command()
 @click.argument('table_name')
@@ -44,7 +48,7 @@ def describe_table(table_name, connection_string, output_file, db_schema, geomet
     storage = create_storage_adaptor(connection_string, db_schema, geometry_support)
     descriptor = storage.describe(table_name)
 
-    with fopen(output_file) as file:
+    with fopen(output_file, 'w') as file:
         json.dump(descriptor, file)
 
 @main.command()
@@ -52,38 +56,46 @@ def describe_table(table_name, connection_string, output_file, db_schema, geomet
 @click.argument('table_schema_path')
 @click.option('--connection-string')
 @click.option('--db-schema')
-@click.option('--index-fields')
+@click.option('--indexes-fields')
 @click.option('--geometry-support')
-def create_table(table_name, table_schema_path, connection_string, db_schema, index_fields, geometry_support):
+def create_table(table_name, table_schema_path, connection_string, db_schema, indexes_fields, geometry_support):
     connection_string = get_connection_string(connection_string)
 
     storage = create_storage_adaptor(connection_string, db_schema, geometry_support)
 
-    with fopen(table_schema_path) as file:
-        table_schema = json.load(file)
+    table_schema = get_table_schema(table_schema_path)
 
-    if index_fields != None:
-        index_fields = index_fields.split(',')
+    if indexes_fields != None:
+        indexes_fields = indexes_fields.split(',')
 
-    storage.create(table_name, table_schema, index_fields=index_fields)
+    storage.create(table_name, table_schema, indexes_fields=indexes_fields)
 
 @main.command()
 @click.argument('table_name')
+@click.option('--table-schema-path')
 @click.option('--connection-string')
 @click.option('-f','--input-file')
 @click.option('--db-schema')
 @click.option('--geometry-support')
-def write(table_name, connection_string, input_file, db_schema, geometry_support):
+def write(table_name, table_schema_path, connection_string, input_file, db_schema, geometry_support):
     connection_string = get_connection_string(connection_string)
 
+    table_schema = get_table_schema(table_schema_path)
+
     storage = create_storage_adaptor(connection_string, db_schema, geometry_support)
+
+    if table_schema_path != None:
+        table_schema = get_table_schema(table_schema_path)
+        storage.describe(table_name, descriptor=table_schema)
+
+    print(storage.describe(table_name))
 
     ## TODO: skip csv header?
     ## TODO: csv settings? use Frictionless Data csv standard?
     ## TODO: support line delimted json?
     with fopen(input_file) as file:
         rows = csv.reader(file)
-        storage.write(table_name, rows, as_generator=True)
+        storage.write(table_name, rows) # ?? as_generator=True
 
 @main.command()
 @click.argument('table_name')
@@ -96,10 +108,10 @@ def read(table_name, connection_string, output_file, db_schema, geometry_support
 
     storage = create_storage_adaptor(connection_string, db_schema, geometry_support)
 
-    ## TODO: skip csv header?
+    ## TODO: csv header?
     ## TODO: csv settings? use Frictionless Data csv standard?
     ## TODO: support line delimted json?
-    with fopen(input_file, mode='wb') as file:
+    with fopen(output_file, mode='w') as file:
         writer = csv.writer(file)
         for row in storage.iter(table_name):
             writer.writerow(row)
