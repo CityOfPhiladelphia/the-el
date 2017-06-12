544c3ddaf037b0a20fc57b025bd684efa6ebcc31
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-06-12 10:13:40 -0400

    Add db specific requirements

--- setup.py
+++ setup.py
@@ -13,6 +13,13 @@ setup(
         'smart_open==1.5.2',
         'jsontableschema_sql==0.8.0',
     ],
+    extras_require={
+        'postgres': ['psycopg2==2.7.1'],
+        'oracle': ['cx-Oracle==5.3'],
+        'mssql': ['pymssql==2.1.3'],
+        'postgis': ['GeoAlchemy2==0.4.0'],
+        'oracle_sde': ['pyproj==1.9.5.1', 'Shapely==1.5.17.post1']
+    },
     dependency_links=[
         'https://github.com/CityOfPhiladelphia/jsontableschema-sql-py/tarball/master#egg=jsontableschema_sql-0.8.0'
     ],
